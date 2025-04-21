d5af561e946c331431e9ef631eebdc7c714b9070
Lahiru Karunarathna <lahirugaveesh3@gmail.com>
2025-04-21 09:27:22 +0530

    Added new changes to Menu and Home

---
+++ Menu.js
@@ -0,0 +1,7 @@
+function mouseover(demo) {
+    document.getElementById("demo").style.color = " #735557";
+  }
+  function mouseout(demo) {
+
+    document.getElementById("demo").style.color = "black"
+  }
